34428244f05d0a2dbe871d6948796f98bf11d6ab
Панков Денис <Pankkovv@yandex.ru>
2024-04-07 19:38:04 +0300

    feat: create a new project

---
+++ src/main/java/ru/pankkovv/auctioneerBot/AuctioneerBotApplication.java
@@ -0,0 +1,13 @@
+package ru.pankkovv.auctioneerBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctioneerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctioneerBotApplication.class, args);
+	}
+
+}
