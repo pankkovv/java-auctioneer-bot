3b7abbc627ded5de3bb6a498a3b4261d840e0a6e
Панков Денис <pankkovv@yandex.ru>
2024-04-30 16:49:11 +0300

    feat:create a project structure

--- src/main/java/ru/pankkovv/auctioneerBot/telegram/service/admin/SettingsCommand.java
+++ src/main/java/ru/pankkovv/auctioneerBot/telegram/service/admin/SettingsCommand.java
@@ -1,13 +1,9 @@
 package ru.pankkovv.auctioneerBot.telegram.service.admin;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
-import ru.pankkovv.auctioneerBot.telegram.nonCommand.Settings;
-import ru.pankkovv.auctioneerBot.telegram.model.Bot;
 import ru.pankkovv.auctioneerBot.telegram.service.Command;
 import ru.pankkovv.auctioneerBot.telegram.utils.Utils;
 
@@ -32,12 +28,12 @@ public class SettingsCommand extends Command {
         Long chatId = chat.getId();
         sendAnswer(absSender, chatId, this.getCommandIdentifier(), userName,
                 "*Текущие настройки*\n" +
-                                "- минимальное число, использующееся в заданиях - *%s*\n" +
-                                "- максимальное число, использующееся в заданиях - *%s*\n" +
-                                "- число страниц итогового файла - *%s*\n\n" +
-                                "Если Вы хотите изменить эти параметры, введите через пробел или запятую 3 числа - " +
-                                "минимальное число, максимальное число и количество страниц в файле (не более 10)\n\n" +
-                                "\uD83D\uDC49 Например, 3,15,6 или 4 17 3");
+                        "- минимальное число, использующееся в заданиях - *%s*\n" +
+                        "- максимальное число, использующееся в заданиях - *%s*\n" +
+                        "- число страниц итогового файла - *%s*\n\n" +
+                        "Если Вы хотите изменить эти параметры, введите через пробел или запятую 3 числа - " +
+                        "минимальное число, максимальное число и количество страниц в файле (не более 10)\n\n" +
+                        "\uD83D\uDC49 Например, 3,15,6 или 4 17 3");
 
         log.debug(String.format("Пользователь %s. Завершено выполнение команды %s", userName,
                 this.getCommandIdentifier()));
